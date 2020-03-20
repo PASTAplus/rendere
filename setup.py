df19e49ff757d6f0f27a22fb5caabe46b0c6e82a
Mark Servilla <mark.servilla@gmail.com>
2020-03-20 15:52:09 -0600

    Add deployment configuration

---
+++ setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: setup.py
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    3/20/2020
+"""
+from os import path
+from setuptools import find_packages, setup
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'LICENSE'), encoding='utf-8') as f:
+    full_license = f.read()
+
+setup(
+    name="rendere",
+    version="2020.03.20",
+    description="Render an EML metadata document into HTML",
+    long_description=long_description,
+    author="PASTA+ project",
+    url="https://github.com/servilla/rendere",
+    license=full_license,
+    packages=find_packages(where="src"),
+    include_package_data=True,
+    exclude_package_data={"": ["settings.py, properties.py, config.py"],},
+    package_dir={"": "src"},
+    python_requires=">3.8.*",
+    install_requires=[
+        "click>=7.1.1",
+        "daiquiri>=2.1.1",
+        "jinja2 >= 2.11.1",
+        "lxml >= 4.5.0",
+        ],
+    entry_points={"console_scripts": ["rendere=rendere.render:main"]},
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+    ],
+)
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
