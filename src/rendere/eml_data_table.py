458ecb897d14e5d4e5cad4875ee71cc4e54463ce
Mark Servilla <mark.servilla@gmail.com>
2020-03-28 21:59:31 -0600

    Extend table physical presentation

--- src/rendere/eml_data_table.py
+++ src/rendere/eml_data_table.py
@@ -28,7 +28,7 @@ def data_table(tables: list) -> list:
         description = table.find("./entityDescription")
         if description is not None:
             t["entityDescription"] = clean(description.xpath("string("))
-        physicals = table.findall("./physical")
+        physicals = table.findall(".//physical")
         if len(physicals) > 0:
             t["physical"] = physical(physicals)
         data_tables.append(t)
@@ -40,19 +40,150 @@ def physical(phys: list) -> list:
     physicals = list()
     for phy in phys:
         p = dict()
-        p["objectName"] = phy.find("./objectName").text.strip()
+        p["Object Name"] = phy.find("./objectName").text.strip()
         size = phy.find("./size")
         if size is not None:
             value = clean(size.xpath("string()"))
             unit = size.attrib["unit"].strip()
-            p["size"] = f"{value} ({unit})"
-        checksums = phy.findall("./authentication")
+            p["Size"] = f"{value} ({unit})"
+        checksums = phy.findall(".//authentication")
         if len(checksums) > 0:
             for checksum in checksums:
                 c = list()
                 value = clean(checksum.xpath("string()"))
                 method = checksum.attrib["method"].strip()
                 c.append(f"{value} ({method})")
-            p["authentication"] = c
+            p["Checksum(s)"] = c
+        compression_methods = phy.findall(".//compressionMethod")
+        if len(compression_methods) > 0:
+            for compression_method in compression_methods:
+                c = list()
+                c.append(clean(compression_method.xpath("string()")))
+            p["compression_method"] = c
+        encoding_methods = phy.findall(".//encodingMethod")
+        if len(encoding_methods) > 0:
+            for encoding_method in encoding_methods:
+                c = list()
+                c.append(clean(encoding_method.xpath("string()")))
+            p["encoding_method"] = c
+        character_encoding = phy.find("./characterEncoding")
+        if character_encoding is not None:
+            value = clean(character_encoding.xpath("string()"))
+            p["characterEncoding"] = value
+        p["Data Format"] = data_format(phy.find("./dataFormat"))
         physicals.append(p)
     return physicals
+
+
+# noinspection DuplicatedCode
+def data_format(df) -> dict:
+    d_format = dict()
+    f = df.getchildren()[0]  # Can only be one child
+    if f.tag == "textFormat":
+        tf = dict()
+        num_header_lines = f.find("./numHeaderLines")
+        if num_header_lines is not None:
+            tf["Header Lines"] = clean(num_header_lines.xpath("string()"))
+        num_footer_lines = f.find("./numFooterLines")
+        if num_footer_lines is not None:
+            tf["Footer Lines"] = clean(num_footer_lines.xpath("string()"))
+        record_delimiters = f.findall("./recordDelimiter")
+        if record_delimiters is not None:
+            rd = list()
+            for record_delimiter in record_delimiters:
+                rd.append(clean(record_delimiter.xpath("string()")))
+            tf["Record Delimiter(s)"] = rd
+        physical_line_delimiters = f.findall(".//physicalLineDelimiter")
+        if len(physical_line_delimiters) > 0:
+            pd = list()
+            for physical_line_delimiter in physical_line_delimiters:
+                pd.append(clean(physical_line_delimiter.xpath("string()")))
+            tf["Physical Line Delimiter(s)"] = pd
+        num_physical_lines_per_record = f.find("./numPhysicalLinesPerRecord")
+        if num_physical_lines_per_record is not None:
+            tf["Physical Lines Per Record"] = \
+                clean(num_physical_lines_per_record.xpath("string()"))
+        max_record_length = f.find("./maxRecordLength")
+        if max_record_length is not None:
+            tf["Maximum Record Length"] = \
+                clean(max_record_length.xpath("string()"))
+        attribute_orientation = f.find("./attributeOrientation")
+        tf["Attribute Orientation"] = \
+            clean(attribute_orientation.xpath("string()"))
+        simple_delimited = f.find("./simpleDelimited")
+        if simple_delimited is not None:
+            td = dict()
+            fd = list()
+            field_delimiters = simple_delimited.findall("./fieldDelimiter")
+            for field_delimiter in field_delimiters:
+                fd.append(clean(field_delimiter.xpath("string()")))
+            td["fieldDelimiter"] = fd
+            collapse_delimiters = simple_delimited.find("./collapseDelimiters")
+            if collapse_delimiters is not None:
+                td["collpaseDelimiters"] = \
+                    clean(collapse_delimiters.xpath("string()"))
+            quote_characters = simple_delimited.findall("./quoteCharacter")
+            if quote_characters is not None:
+                qc = list()
+                for quote_character in quote_characters:
+                    qc.append(clean(quote_character.xpath("string()")))
+                td["quoteCharacter"] = qc
+            literal_characters = simple_delimited.findall("./literalCharacter")
+            if literal_characters is not None:
+                lc = list()
+                for literal_character in literal_characters:
+                    lc.append(clean(literal_character.xpath("string()")))
+                td["literalCharacter"] = lc
+        complex = f.find("./complex")
+        if complex is not None:
+            c = list()
+            c_children = complex.getchildren()
+            for c_child in c_children:
+                if c_child == "textFixed":
+                    tf = dict()
+                    field_width = c_child.find("./fieldWidth")
+                    tf["fieldWidth"] = clean(field_width.xpath("string()"))
+                    line_number = c_child.find("./lineNumber")
+                    if line_number is not None:
+                        tf["lineNumber"] = \
+                            clean(line_number.xpath("string()"))
+                    field_start_column = c_child.find("./fieldStartColumn")
+                    if field_start_column is not None:
+                        tf["fieldStartColumn"] = \
+                            clean(field_start_column.xpath("string()"))
+                    c.append({"textFixed": tf})
+                else:  # c_child == "textDelimited"
+                    td = dict()
+                    field_delimiter = c_child.find("./fieldDelimiter")
+                    td["fieldDelimiter"] = \
+                        clean(field_delimiter.xpath("string()"))
+                    collapse_delimiters = \
+                        c_child.find("./collapseDelimiters")
+                    if collapse_delimiters is not None:
+                        td["collpaseDelimiters"] = \
+                            clean(collapse_delimiters.xpath("string()"))
+                    line_number = c_child.find("./lineNumber")
+                    if line_number is not None:
+                        td["lineNumber"] = clean(
+                            line_number.xpath("string()"))
+                    quote_characters = c_child.findall("./quoteCharacter")
+                    if quote_characters is not None:
+                        qc = list()
+                        for quote_character in quote_characters:
+                            qc.append(
+                                clean(quote_character.xpath("string()")))
+                        td["quoteCharacter"] = qc
+                    literal_characters = c_child.findall("./literalCharacter")
+                    if literal_characters is not None:
+                        lc = list()
+                        for literal_character in literal_characters:
+                            lc.append(
+                                clean(literal_character.xpath("string()")))
+                        td["literalCharacter"] = lc
+                    c.append({"textDelimited": td})
+        d_format["Text"] = tf
+    elif f.tag == "externallyDefinedFormat":
+        pass
+    else:  # f.tag == binaryRasterFormat
+        pass
+    return d_format
