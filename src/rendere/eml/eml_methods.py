0d363a2ad6bd19139d7991fba7fae24cee461dfc
Mark Servilla <mark.servilla@gmail.com>
2020-04-01 16:27:01 -0600

    Add methodStep parsing

--- src/rendere/eml/eml_methods.py
+++ src/rendere/eml/eml_methods.py
@@ -14,6 +14,10 @@
 """
 import daiquiri
 
+from rendere.eml.eml_text import eml_text, html_text
+from rendere.eml.eml_utils import clean
+
+
 logger = daiquiri.getLogger(__name__)
 
 
@@ -22,9 +26,28 @@ def eml_methods(element_obj) -> list:
     children = element_obj.getchildren()
     for child in children:
         if child.tag == "methodStep":
-            methods.append({"Method Step": "method step"})
+            methods.append({"Method Step": eml_method_step(child)})
         elif child.tag == "sampling":
             methods.append({"Sampling": "sampling"})
         else:  # child.tag == "qualityControl"
             methods.append({"Quality Control": "quality control"})
     return methods
+
+
+def eml_method_step(element_obj) -> dict:
+    method_step = dict()
+    description = element_obj.find("./description")
+    method_step["Description"] = html_text(eml_text(description))
+    instrumentation = element_obj.findall("./instrumentation")
+    if len(instrumentation) > 0:
+        i = list()
+        for instrument in instrumentation:
+            i.append(clean(instrument.xpath("string()")))
+        method_step["Instrumentation"] = i
+    substep = element_obj.findall("./subStep")
+    if len(substep) > 0:
+        s = list()
+        for step in substep:
+            s.append(eml_method_step(step))
+        method_step["Sub Step(s)"] = s
+    return method_step
